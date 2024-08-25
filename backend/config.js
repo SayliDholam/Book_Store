3193aa88513815f0a7e7f177054e6a802cfdc365
Sayli R Dholam <saylidholam08@gmail.com>
2024-08-25 16:43:26 +0530

    configure port, db connection and request

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const MONGODB_URI = `mongodb://127.0.0.1:27017/`;
+
+export const DB_NAME = 'BookStore';
+export const COLLECTION_NAME = 'Data1';
--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,26 @@
 import express from "express";
+import { PORT , MONGODB_URI, DB_NAME, COLLECTION_NAME} from "./config.js";
+import mongoose from "mongoose";
 
+
+const app = express();
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send("Hello");
+});
+
+
+mongoose.connect(MONGODB_URI).then(() => 
+    {
+        console.log('db connect done');
+        app.listen(PORT, () => {
+            console.log(`listen to ${PORT}`);
+        });
+    })
+    .catch((error) =>
+    {
+        console.log(error);
+    });
+
+    
