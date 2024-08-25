3ce2d7c66bb3d5a353c30c9b7aa117d1c827bcf4
Sayli R Dholam <saylidholam08@gmail.com>
2024-08-25 19:47:57 +0530

    code pages and components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,24 +1,27 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Spinner from '../components/Spinner';
-import {Link} from 'react-router-dom';
-import {AiOutLineEdit} from 'react-icons/ai';
-import { InfoCircle } from 'react-icons/bs';
-import { MdOutLineAddBox, MdOutlineDelete } from 'react-icons/md';
-
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import BooksTable from '../components/home/BooksTable';
+import BooksCard from '../components/home/BooksCard';
 
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState('table');
 
   useEffect(() => {
     setLoading(true);
-    axios.get('http://localhost:5555/books')
-      .then(response => { 
+    axios
+      .get('http://localhost:5555/books')
+      .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
       })
-      .catch(error => { 
+      .catch((error) => {
         console.log(error);
         setLoading(false);
       });
@@ -57,4 +60,4 @@ const Home = () => {
   );
 };
 
-export default Home
+export default Home;
