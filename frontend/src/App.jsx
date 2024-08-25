8c94e78949e0ddde2965e069aa7ba20d1856677c
Sayli R Dholam <saylidholam08@gmail.com>
2024-08-25 18:50:09 +0530

    setup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,11 +12,11 @@ const App = () =>
 {
   return (
     <Routes>
-      <Route path ='' element = {d} />
-      <Route path ='' element = {d} />
-      <Route path ='' element = {d} />
-      <Route path ='' element = {d} />
-      <Route path ='' element = {d} />
+      <Route path ='/' element = {<Home />} />
+      <Route path ='/books/create' element = {<CreateBook/>} />
+      <Route path ='/books/details/:id' element = {<ShowBook/>} />
+      <Route path ='/books/edit/:id' element = {<EditBook/>} />
+      <Route path ='/books/delete/:id' element = {<DeleteBook/>} />
     </Routes>
   )
 }
