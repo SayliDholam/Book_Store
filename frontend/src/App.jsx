7457c6a3206c865dec616e6d7af18e5263041b0e
Sayli R Dholam <saylidholam08@gmail.com>
2024-08-25 18:46:45 +0530

    Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-
+import Home from './pages/Home'
+import CreateBook from './pages/CreateBook'
+import ShowBooks from './pages/ShowBooks'
+import EditBook from './pages/EditBook'
+import DeleteBook from './pages/DeleteBook'
 
 
 
